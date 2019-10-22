f6ead6d1780a142979cff77bdd3ec9642070673c
jwpark06 <rusheran@naver.com>
2019-10-23 01:42:36 +0900

    Create Customer Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'; 
+import Customer from './components/Customer'; // Customer Class는 App Class의 외부에 있기 때문에, import 해주어야 한다.
 import './App.css';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customer = {
+  'name': '이순신',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+}
+
+class App extends Component {
+  render() {
+    return (
+      <Customer
+        name={customer.name} // Customer 컴포넌트는 이러한 방식으로 Props를 전달받을 수 있음.
+        birthday={customer.birthday}
+        gender={customer.gender}
+        job={customer.job}
+      /> // App 컴포넌트 안에 Customer 컴포넌트가 포함된 형태로 화면에 출력 될것임.
+
+    );
+  }
 }
 
 export default App;
+
+// import해주고, render 내부에 Customer 컴포넌트 태그를 넣었다면
+// 이로써 Customer 컴포넌트를 불러와서 그것을 App 컴포넌트 안에서 출력할 수 있도록 만든 것임.
---
+++ src/components/Customer.js
@@ -0,0 +1,27 @@
+import React from 'react'; // 리액트 라이브러리를 불러올 수 있게 하는 것.
+
+// Customer 클래스를 정의하는데,
+// Customer 클래스는 리액트의 컴포넌트로 작성이 된 클래스임을 선언한다.
+// 즉, React.Component는 일종의 라이브러리이자 클래스라고 할 수 있다.
+class Customer extends React.Component {
+    
+    // React.Component 라이브러리에 render란 함수가 있다.
+    // render는 항상 수행되는 것으로써,
+    // Customer 라는 컴포넌트를 실제 화면에 그리고자 할 때,
+    // 실제로 그려지는 내용이 담기게 된다.
+    render() {
+        // props는 React.component가 포함하고 있는 내용이므로 this 키워드를 사용할 수 있음.
+        // 전달받은 프로퍼티를 사용할 때 this.props.----를 사용한 것임.
+        return (
+            <div>
+                <h2>{this.props.name}</h2>
+                <p>{this.props.birthday}</p>
+                <p>{this.props.gender}</p>
+                <p>{this.props.job}</p>
+            </div>
+        );
+    }
+    // Customer라는 컴포넌트의 작성이 완료되었다. App.js에서 이제 이 Customer 컴포넌트를 쓰자.
+}
+
+export default Customer; // 다른 라이브러리가 Customer 라이브러리를 쓸수 있게 내보내는 것.
