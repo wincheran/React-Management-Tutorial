b64cfd1e370d78fae88c74b847e8cd517a1f13b7
jwpark06 <rusheran@naver.com>
2019-10-29 22:29:41 +0900

    Apply Material UI

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import Customer from './components/Customer'; // Customer Class는 App Class의 외부에 있기 때문에, import 해주어야 한다.
 import './App.css';
+import Paper from '@material-ui/core/Paper'; // 페이퍼 컴포넌트는 어떠한 컴포넌트의 외부를 감싸기 위해 사용하는 라이브러리 (그냥 div대신 쓰는 느낌임..))
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableBody from '@material-ui/core/TableBody';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import { withStyles } from '@material-ui/core/styles'; // 스타일 라이브러리 호출
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3, // 가중치 3
+    overflowX: "auto"
+  },
+  table: {
+    minWidth: 1080
+  }
+});
 
 const customers = [{
   'id': 1,
@@ -30,30 +48,47 @@ const customers = [{
 
 class App extends Component {
   render() {
+    const { classes } = this.props; // 변수 생성하고, 위에서 만든 스타일이 적용되게 할 예정.
+
     return (
       // App 컴포넌트 안에 Customer 컴포넌트가 포함된 형태로 화면에 출력 될것임.
-      <div>
-        {
-          customers.map(oCustomerInfo => { // 여기서 aCustomerInfo는 Customers 배열의 원소 하나하나를 의미
-            return (
-              <Customer
-                key={oCustomerInfo.id} // 메서드 map을 사용하려면 각 원소를 구분할 수 있는 'key를 반드시 넣어줘야 함'
-                id={oCustomerInfo.id}
-                image={oCustomerInfo.image}
-                name={oCustomerInfo.name} // Customer 컴포넌트는 이러한 방식으로 Props를 전달받을 수 있음.
-                birthday={oCustomerInfo.birthday}
-                gender={oCustomerInfo.gender}
-                job={oCustomerInfo.job}
-              />
-            );
-          })
-        }
-      </div>
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {
+              customers.map(oCustomerInfo => { // 여기서 aCustomerInfo는 Customers 배열의 원소 하나하나를 의미
+                return (
+                  <Customer
+                    key={oCustomerInfo.id} // 메서드 map을 사용하려면 각 원소를 구분할 수 있는 'key를 반드시 넣어줘야 함'
+                    id={oCustomerInfo.id}
+                    image={oCustomerInfo.image}
+                    name={oCustomerInfo.name} // Customer 컴포넌트는 이러한 방식으로 Props를 전달받을 수 있음.
+                    birthday={oCustomerInfo.birthday}
+                    gender={oCustomerInfo.gender}
+                    job={oCustomerInfo.job}
+                  />
+
+                );
+              })
+            }
+          </TableBody>
+        </Table>
+      </Paper>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App); // style를 적용해서 App을 내보내는 것
 
 // import해주고, render 내부에 Customer 컴포넌트 태그를 넣었다면
 // 이로써 Customer 컴포넌트를 불러와서 그것을 App 컴포넌트 안에서 출력할 수 있도록 만든 것임.
--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,10 +1,12 @@
 import React from 'react'; // 리액트 라이브러리를 불러올 수 있게 하는 것.
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 // Customer 클래스를 정의하는데,
 // Customer 클래스는 리액트의 컴포넌트로 작성이 된 클래스임을 선언한다.
 // 즉, React.Component는 일종의 라이브러리이자 클래스라고 할 수 있다.
 class Customer extends React.Component {
-    
+
     // React.Component 라이브러리에 render란 함수가 있다.
     // render는 항상 수행되는 것으로써,
     // Customer 라는 컴포넌트를 실제 화면에 그리고자 할 때,
@@ -13,37 +15,17 @@ class Customer extends React.Component {
         // props는 React.component가 포함하고 있는 내용이므로 this 키워드를 사용할 수 있음.
         // 전달받은 프로퍼티를 사용할 때 this.props.----를 사용한 것임.
         return (
-            <div>
-                <CustomerProfile id={this.props.id} name={this.props.name} image={this.props.image} />
-                <CustomerInfo birthday={this.props.birthday} gender={this.props.gender} job={this.props.job} />
-            </div>
-        );
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile" /></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
+        )
     }
     // Customer라는 컴포넌트의 작성이 완료되었다. App.js에서 이제 이 Customer 컴포넌트를 쓰자.
-}
-
-// 하나의 컴포넌트를 추가로 정의하자
-class CustomerProfile extends React.Component {
-    render() {
-        return (
-            <div>
-                <img src={this.props.image} alt="profile" />
-                <h2>{this.props.name}({this.props.id})</h2>
-            </div>
-        );
-    }
-}
-
-class CustomerInfo extends React.Component {
-    render() {
-        return (
-            <div>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
-        );
-    }
 }
 
 export default Customer; // 다른 라이브러리가 Customer 라이브러리를 쓸수 있게 내보내는 것.
