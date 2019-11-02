9e0450186e8837a30521bb380424e55f7143bc22
jwpark06 <rusheran@naver.com>
2019-11-02 17:59:24 +0900

    Craete Customer Database Table & Node.js Database Connection Settings

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,8 +7,35 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json()); // REST API는 json 타입을 이용해서 데이터를 주고 받는다.
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const data = fs.readFileSync('./database.json'); // db 설정파일 호출
+const conf = JSON.parse(data);
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.password,
+    port: conf.port,
+    database: conf.database
+});
+connection.connect();
+
 app.get('/api/customers', (req, res) => {
-    res.send([
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) => {
+            res.send(rows);
+        }
+    );
+    // query(param1, param2)
+    // param1: 실제로 동작할 쿼리
+    // param2: callback function.
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+/*
+DB에 넣어둘 값
+[
         {
             'id': 1,
             'image': 'https://placeimg.com/64/64/any',
@@ -32,7 +60,5 @@ app.get('/api/customers', (req, res) => {
             'gender': '남자',
             'job': '대학생'
         }
-    ]);
-});
-
-app.listen(port, () => console.log(`Listening on port ${port}`));
+    ]
+    */
